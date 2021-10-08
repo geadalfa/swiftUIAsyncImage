088b441bdf61613431459124e704d99978360f75
geadalfa <ggeadalfa@gmail.com>
2021-10-08 15:28:18 +0700

    Initial Commit

---
+++ swiftUIAsyncImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIAsyncImage
+//
+//  Created by Geadalfa Giyanda on 08/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIAsyncImage/swiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIAsyncImageApp.swift
+//  swiftUIAsyncImage
+//
+//  Created by Geadalfa Giyanda on 08/10/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
